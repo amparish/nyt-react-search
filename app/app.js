e60e5a6e85cf27c9bd89bd024e1899b6b52d4d57
amparish <alexmparish@gmail.com>
2017-11-08 16:39:54 -0600

    rebuilt, complete

--- app/app.js
+++
@@ -1,9 +0,0 @@
-// Include the React Dependencies
-var React = require("react");
-var ReactDOM = require("react-dom");
-
-// Include react-router routes file
-var routes = require("./routes");
-
-// This code here allows us to render our main component
-ReactDOM.render(routes, document.getElementById("app"));
